261014194e7e00be1995e53dfd28a9fa14920eab
shadman8082 <shahriersadman670@gmail.com>
2025-12-05 02:17:40 +0600

    first

---
+++ src/main/java/com/example/cse213_finalproject_groupno77_texttilefact/GenerateReportsController.java
@@ -0,0 +1,43 @@
+package com.example.cse213_finalproject_groupno77_texttilefact;
+
+public class GenerateReportsController
+{
+    @javafx.fxml.FXML
+    private Button btnExportPDF;
+    @javafx.fxml.FXML
+    private Button btnSave;
+    @javafx.fxml.FXML
+    private ComboBox comboReportType;
+    @javafx.fxml.FXML
+    private Label lblGenerateStatus;
+    @javafx.fxml.FXML
+    private Button btnPrint;
+    @javafx.fxml.FXML
+    private DatePicker dateTo;
+    @javafx.fxml.FXML
+    private DatePicker dateFrom;
+    @javafx.fxml.FXML
+    private Button btnGenerate;
+    @javafx.fxml.FXML
+    private TextArea txtReportPreview;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleExportPDF(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handlePrint(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleGenerate(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/cse213_finalproject_groupno77_texttilefact/LogisticManagerDashBoardController.java
@@ -0,0 +1,44 @@
+package com.example.cse213_finalproject_groupno77_texttilefact;
+
+public class LogisticManagerDashBoardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void TrackShipementsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void VehicleRecordsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CommuniacationOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ManageSchedulesOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void InventoryTransferOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void StuffPerformanceOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenarateReportsOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OptimizeroutesOnActionButton(ActionEvent actionEvent) {
+    }
+}
