a9b65686e98ab94c693dfb26e4708fa64e07d61b
shadman8082 <shahriersadman670@gmail.com>
2025-12-05 01:36:42 +0600

    Initial commit

---
+++ src/main/java/com/example/cse213_finalproject_groupno77_texttilefact/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cse213_finalproject_groupno77_texttilefact;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cse213_finalproject_groupno77_texttilefact {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cse213_finalproject_groupno77_texttilefact to javafx.fxml;
+    exports com.example.cse213_finalproject_groupno77_texttilefact;
+}
