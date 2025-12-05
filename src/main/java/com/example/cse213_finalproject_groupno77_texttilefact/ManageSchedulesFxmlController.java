e8cf0de682e57ff87580848c4a82e419c578c71c
shadman8082 <shahriersadman670@gmail.com>
2025-12-05 15:35:03 +0600

    work 2

---
+++ src/main/java/com/example/cse213_finalproject_groupno77_texttilefact/ManageSchedulesFxmlController.java
@@ -0,0 +1,43 @@
+package com.example.cse213_finalproject_groupno77_texttilefact;
+
+import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+
+public class ManageSchedulesFxmlController
+{
+    @javafx.fxml.FXML
+    private TableColumn DriverTableViewUpcomingColBox;
+    @javafx.fxml.FXML
+    private TableView UpcomingSchedulesTableView;
+    @javafx.fxml.FXML
+    private TableColumn DateTableViewUpcomingColBox;
+    @javafx.fxml.FXML
+    private TableColumn StatusTableViewUpcomingColBox;
+    @javafx.fxml.FXML
+    private TextField DestinationTextFiled;
+    @javafx.fxml.FXML
+    private DatePicker DeliveryDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField ClientNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox SelectDriverComboBox;
+    @javafx.fxml.FXML
+    private TableColumn ClientTableViewUpcomingColBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void CancelOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void EditOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CreateScheduleonActionButton(ActionEvent actionEvent) {
+    }
+}
