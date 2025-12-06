aae8fad59794052b1a116cb12cb0591c553889a1
shadman8082 <shahriersadman670@gmail.com>
2025-12-06 14:16:48 +0600

    work 3

---
+++ src/main/java/com/example/cse213_finalproject_groupno77_texttilefact/OptimizeRoutesController.java
@@ -0,0 +1,37 @@
+package com.example.cse213_finalproject_groupno77_texttilefact;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.awt.event.ActionEvent;
+
+public class OptimizeRoutesController
+{
+    @javafx.fxml.FXML
+    private Button btnSaveRoute;
+    @javafx.fxml.FXML
+    private Label lblTime;
+    @javafx.fxml.FXML
+    private Button btnFindRoute;
+    @javafx.fxml.FXML
+    private Label lblDistance;
+    @javafx.fxml.FXML
+    private TextArea txtAddresses;
+    @javafx.fxml.FXML
+    private Label lblRouteStatus;
+    @javafx.fxml.FXML
+    private TextArea txtRouteResult;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSaveRoute(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleFindRoute(ActionEvent actionEvent) {
+    }
+}
